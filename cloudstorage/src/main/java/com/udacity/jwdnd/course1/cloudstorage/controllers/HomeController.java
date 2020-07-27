f0c914459c21003bc9b4ba5ec3993ff0bbc5b69d
Marta Krzystanek <marta.krzystanek@vimn.com>
2020-07-27 11:26:40 +0200

    handle file upload

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/home")
@@ -10,6 +14,12 @@ public class HomeController {
 
     @GetMapping
     public String getHome() {
+        return "home";
+    }
+
+    @PostMapping
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model) {
+        model.addAttribute("file", file);
         return "home";
     }
 }
